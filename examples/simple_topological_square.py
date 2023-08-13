40b6218df937487101d48d50213283582a456279
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2023-08-13 13:58:44 +1000

    Added simple topological square example

---
+++ examples/simple_topological_square.py
@@ -0,0 +1,24 @@
+
+from cpsvis.core.topology import TopologicalEdge, TopologicalVertex, TopologicalPolygon
+
+polygon = TopologicalPolygon()
+
+
+v0 = TopologicalVertex()
+v1 = TopologicalVertex()
+v2 = TopologicalVertex()
+v3 = TopologicalVertex()
+
+e1 = TopologicalEdge(v0,v1)
+e2 = TopologicalEdge(v1, v2)
+e3 = TopologicalEdge(v2, v3)
+e4 = TopologicalEdge(v3, v0)
+
+polygon.add_disjoint_edge(e1)
+polygon.add_vertex_connected_edge(e1, e2, v1)
+polygon.add_vertex_connected_edge(e2, e3, v2)
+polygon.add_vertex_connected_edge(e3, e4, v3)
+e4.add_neighbouring_edge(e1, v0)
+
+print(polygon.check_closed())
+
